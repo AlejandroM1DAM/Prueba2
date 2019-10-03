899e0a705f0e091146dac45d289dcbf6af7e5b64
Alejandro Muñiz <nadie.2899@gmail.com>
2019-10-03 13:19:28 +0200

    Ahora muestra la localidad actual en la sigiente linea de la ciudad de nacimiento. Guardado

--- Prueba.java
+++ Prueba.java
@@ -1,24 +1,24 @@
 public class Prueba {
-private String datos = "";
-	private String nombCompleto = "";
-	private String ciudadNacimiento = "";
-	private String localidadActual = "";
-	public Prueba() {
-		
-	}
-	public String nombreCompleto(String nombre, String apellido1, String apellido2) {
-		concat(nombre, " ", apellido1, " ", apellido2)= nombCompleto;
-		return nombCompleto;
-	}
-	public String ciudadNacim(String ciudad) {
-		ciudad = ciudadNacimiento;
-		return ciudadNacimiento;
-	}
-	public String localidadActual(String localidad) {
-		localidad = localidadActual;
-		return localidadActual;
-	}
-	public String toString() {
-		return nombCompleto + "\n" +  ciudadNacimiento + "\n" +  localidadActual;
-	}
+    private String datos = "";
+    private String nombCompleto = "";
+    private String ciudadNacimiento = "";
+    private String localidadActual = "";
+    public Prueba() {
+        
+    }
+    public String nombreCompleto(String nombre, String apellido1, String apellido2) {
+        nombCompleto = nombre + " " + apellido1 + " " + apellido2; 
+        return nombCompleto;
+    }
+    public String ciudadNacim(String ciudad) {
+        ciudad = ciudadNacimiento;
+        return ciudadNacimiento;
+    }
+    public String localidadActual(String localidad) {
+        localidad = localidadActual;
+        return localidadActual;
+    }
+    public String toString() {
+        return nombCompleto + "\n" +  ciudadNacimiento + "\n" +  localidadActual;
+    }
 }
