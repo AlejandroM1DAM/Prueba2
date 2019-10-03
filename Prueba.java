1a0fec094844a6f584205f56fa13d66bc12b9329
Alejandro Muñiz <nadie.2899@gmail.com>
2019-10-03 13:11:56 +0200

    Ahora muestra la ciudad de nacimiento en la sigiente linea del nombre completo

--- Prueba.java
+++ Prueba.java
@@ -1,10 +1,19 @@
 public class Prueba {
+private String datos = "";
 	private String nombCompleto = "";
-	public Prueba(int nombCompleto) {
-		this.nombCompleto = nombCompleto;
+	private String ciudadNacimiento = "";
+	public Prueba() {
+		
 	}
-	public int datos(String nombre, String apellido1, String apellido2) {
-		concat(nombre, " ", apellido1, " ", apellido2)= result;
+	public String nombreCompleto(String nombre, String apellido1, String apellido2) {
+		concat(nombre, " ", apellido1, " ", apellido2)= nombCompleto;
 		return nombCompleto;
+	}
+	public String ciudadNacim(String ciudad) {
+		ciudad = ciudadNacimiento;
+		return ciudadNacimiento;
+	}
+	public String toString() {
+		return nombCompleto + "\n" +  ciudadNacimiento;
 	}
 }
