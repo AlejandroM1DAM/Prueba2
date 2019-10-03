2229680ac1668a45bcdf018ca14792ca6823ee53
Alejandro Muñiz <nadie.2899@gmail.com>
2019-10-03 13:05:51 +0200

    Ahora me genera el nombre completo

---
+++ Prueba.java
@@ -0,0 +1,10 @@
+public class Prueba {
+	private String nombCompleto = "";
+	public Prueba(int nombCompleto) {
+		this.nombCompleto = nombCompleto;
+	}
+	public int datos(String nombre, String apellido1, String apellido2) {
+		concat(nombre, " ", apellido1, " ", apellido2)= result;
+		return nombCompleto;
+	}
+}
