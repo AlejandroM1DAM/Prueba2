62d531049c0e7713b77b2bf7a9021dbdd482d531
Alejandro Muñiz <nadie.2899@gmail.com>
2019-10-03 13:27:07 +0200

    Ahora muestra una cuarta linea con la fecha de nacimiento

--- Prueba.java
+++ Prueba.java
@@ -3,22 +3,34 @@ public class Prueba {
     private String nombCompleto = "";
     private String ciudadNacimiento = "";
     private String localidadActual = "";
+    private String anhoNacim = "";
+    
+    
     public Prueba() {
         
     }
+    
     public String nombreCompleto(String nombre, String apellido1, String apellido2) {
         nombCompleto = nombre + " " + apellido1 + " " + apellido2; 
         return nombCompleto;
     }
+    
     public String ciudadNacim(String ciudad) {
         ciudad = ciudadNacimiento;
         return ciudadNacimiento;
     }
+    
     public String localidadActual(String localidad) {
         localidad = localidadActual;
         return localidadActual;
     }
+    
+    public String anhoNacimiento(int dia, int mes, int anho) {
+        anhoNacim = dia + "/" + mes + "/" + anho; 
+        return anhoNacim;
+    }
+    
     public String toString() {
-        return nombCompleto + "\n" +  ciudadNacimiento + "\n" +  localidadActual;
+        return nombCompleto + "\n" +  ciudadNacimiento + "\n" +  localidadActual + "\n" +  anhoNacim;
     }
 }
